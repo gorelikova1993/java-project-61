e8e888775daf13376f2896b491eff84233faa94a
Yulia Baranova <yulia.gorelikova@yandex.ru>
2024-04-27 21:21:40 +0300

    checkstyle error

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -2,6 +2,8 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class EvenGame {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 10;
 
     public static void startGame() {
         String rules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
@@ -16,7 +18,7 @@ public class EvenGame {
 
     }
     private static String[] generateRound() {
-        var question = Engine.rnd(1, 10);
+        var question = Engine.rnd(MIN_RANGE, MAX_RANGE);
         var correctAnswer = isEven(question) ? "yes" : "no";
         return new String[]{String.valueOf(question), correctAnswer};
     }
